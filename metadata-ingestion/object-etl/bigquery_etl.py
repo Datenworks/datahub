25d2b0d2661bab63535381f5a032bf6e51808b91
Richard Lopes <richard.lopes@datenworks.com>
2020-09-08 14:12:46 -0300

    remove path

--- metadata-ingestion/object-etl/bigquery_etl.py
+++ metadata-ingestion/object-etl/bigquery_etl.py
@@ -2,7 +2,7 @@ from common import run
 
 # See https://github.com/mxmzdlv/pybigquery/ for more details
 URL = 'bigquery://dev-semesp-datascience/stage' # e.g. bigquery://project_id
-OPTIONS = {"credentials_path": "/home/richard/datenworks/projects/datahub/metadata-ingestion/sql-etl/account.json"} # e.g. {"credentials_path": "/path/to/keyfile.json"}
+OPTIONS = {"credentials_path": "<path_credentials>"} # e.g. {"credentials_path": "/path/to/keyfile.json"}
 PLATFORM = 'bigquery'
 
 run(URL, OPTIONS, PLATFORM)
